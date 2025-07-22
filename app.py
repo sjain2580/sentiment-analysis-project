0954e2ac544e20534ea616af4dd7c351fe23ee5e
Sakshi Jain <sjain040395@gmail.com>
2025-07-22 12:41:48 +0530

    updated Procfile

--- app.py
+++ app.py
@@ -95,5 +95,5 @@ def analyze_review():
       return jsonify(results)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
     
